7f4df825f409bf863156b709ad14266ffff54ae1
VictorAlvesD <victoralvesrdias@gmail.com>
2024-04-05 00:54:01 -0300

    Cliente

--- src/main/java/com/casefy/dto/Cliente/ClienteDTO.java
+++ src/main/java/com/casefy/dto/Cliente/ClienteDTO.java
@@ -3,8 +3,8 @@ package com.casefy.dto.Cliente;
 import java.util.Date;
 import java.util.List;
 
-import com.casefy.dto.Endereco.*;
-import com.casefy.dto.Telefone.*;
+import com.casefy.model.Endereco;
+import com.casefy.model.Telefone;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -16,7 +16,7 @@ public record ClienteDTO(
         @NotBlank(message = "O campo senha não pode ser nulo.") String senha,
         @NotBlank(message = "O campo CPF não pode ser nulo.") @Pattern(regexp = "^(\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2})$", message = "CPF inválido") String cpf,
         Date dataNascimento,
-        List<TelefoneDTO> listaTelefone,
-        List<EnderecoDTO> listaEndereco) {
+        List<Telefone> listaTelefone,
+        List<Endereco> listaEndereco) {
 
 }
